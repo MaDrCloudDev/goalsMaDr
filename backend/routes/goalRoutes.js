c5157a8de2879e9821ef623af43351480726406d
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2025-09-14 13:27:21 -0700

    fix goalRoutes for bun
    fix goalRoutes for bun

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -8,22 +8,6 @@ import {
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = new Hono();
-const express = require('express');
-const router = express.Router();
-const {
-	getGoals,
-	setGoal,
-	updateGoal,
-	deleteGoal,
-} = require('../controllers/goalController');
-
-const { protect } = require('../middleware/authMiddleware');
-router.route('/').get(protect, getGoals).post(protect, setGoal);
-router
-	.route('/:id')
-	.delete(protect, deleteGoal)
-	.put(protect, updateGoal);
-module.exports = router;
 
 router.get('/', protect, getGoals);
 router.post('/', protect, setGoal);
